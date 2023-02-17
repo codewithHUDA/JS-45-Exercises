fe9ec3c0356c64eb7860f0986e30d5a3c1df0a37
Huda <hudaverse2005@gmmail.com>
2023-02-17 19:50:51 +0500

    JS exercises

---
+++ Exercise-01/index2.js
@@ -0,0 +1,22 @@
+//Favorite Fruit.
+let favorite_fruits = ['apple', 'banana', 'mango'];
+
+if (favorite_fruits.includes('apple')) {
+  console.log("I really like apples!");
+}
+
+if (favorite_fruits.includes('banana')) {
+  console.log("I really like bananas!");
+}
+
+if (favorite_fruits.includes('mango')) {
+  console.log("I really like mangoes!");
+}
+
+if (favorite_fruits.includes('orange')) {
+  console.log("I really like oranges!");
+}
+
+if (favorite_fruits.includes('pineapple')) {
+  console.log("I really like pineapples!");
+}
